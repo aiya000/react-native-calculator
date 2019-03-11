b6c6b7b6952e5d07476ea7ec4f782f6c1d796655
aiya000 <aiya000.develop@gmail.com>
2019-03-11 19:01:33 +0900

    :+1: Fix index.js to read App

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './build/src/index';
+import App from './build/src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
