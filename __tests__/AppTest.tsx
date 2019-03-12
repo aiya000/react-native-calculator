777acdd5c903449cec4ae84b58f798ecd76c625e
aiya000 <aiya000.develop@gmail.com>
2019-03-12 11:10:51 +0900

    :+1: Add tests

--- __tests__/AppTest.tsx
+++ __tests__/AppTest.tsx
@@ -1,10 +1,42 @@
 import 'jest'
-import * as React from 'react'
-import App from '../src/App'
+import * as keyPad from './KeyPad'
 
-// Note: test renderer must be required after react-native.
-import * as Renderer from 'react-test-renderer'
+it('calculates 1 + 1 to 2', () => {
+  const app = keyPad.pushed(['1', '+', '1', '='])
+  expect(app.instance.state.result).toBe(2)
+})
 
-it('calculates 1 + 1 via UI', () => {
-  Renderer.create(<App />)
+it('calculates + 1 to 1', () => {
+  const app = keyPad.pushed(['+', '1', '='])
+  expect(app.instance.state.result).toBe(1)
+})
+
+it('calculates 1 + 1 + 1 to 3', () => {
+  const app = keyPad.pushed(['1', '+', '1', '+', '1', '='])
+  expect(app.instance.state.result).toBe(2)
+})
+
+it('calculates + 42 to 42', () => {
+  const app = keyPad.pushed(['+', '4', '2', '='])
+  expect(app.instance.state.result).toBe(42)
+})
+
+it('determines the result 2 with 1 + 1 + without =', () => {
+  const app = keyPad.pushed(['1', '+', '1', '+'])
+  expect(app.instance.state.result).toBe(2)
+})
+
+it('passes an unnecessary input + of +-1=', () => {
+  const app = keyPad.pushed(['+', '-', '1', '='])
+  expect(app.instance.state.result).toBe(-1)
+})
+
+it('clears the result with C', () => {
+  const app = keyPad.pushed(['1', 'C'])
+  expect(app.instance.state.result).toBe(0)
+})
+
+it('throws no exception with divide by zero', () => {
+  const app = keyPad.pushed(['1', '/', '0'])
+  expect(app.instance.state.result).toBe(NaN)
 })
---
+++ __tests__/KeyPad.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react' // needed by react-test-renderer
+import * as Renderer from 'react-test-renderer'
+import App from '../src/App'
+import { Input } from '../src/data'
+import { KeyPad } from '../src/KeyPad'
+import { ReactTestInstance } from 'react-test-renderer'
+import { View, TouchableHighlight, Text } from 'react-native'
+
+export interface Pads {
+  '1': ReactTestInstance, '2': ReactTestInstance, '3': ReactTestInstance, '+': ReactTestInstance,
+  '4': ReactTestInstance, '5': ReactTestInstance, '6': ReactTestInstance, '-': ReactTestInstance,
+  '7': ReactTestInstance, '8': ReactTestInstance, '9': ReactTestInstance, '*': ReactTestInstance,
+  'C': ReactTestInstance, '0': ReactTestInstance, '=': ReactTestInstance, '/': ReactTestInstance,
+}
+
+export type Button = keyof Pads
+
+function makePads(app: ReactTestInstance): Pads {
+  const rows = app
+    .findByType(KeyPad)
+    .findAllByType(View)[0]
+    .findAllByType(View)
+  const row1 = rows[0].findAllByType(TouchableHighlight)
+  const row2 = rows[1].findAllByType(TouchableHighlight)
+  const row3 = rows[2].findAllByType(TouchableHighlight)
+  const row4 = rows[3].findAllByType(TouchableHighlight)
+
+  return {
+    '1': row1[0], '2': row1[1], '3': row1[2], '+': row1[3],
+    '4': row2[0], '5': row2[1], '6': row2[2], '-': row2[3],
+    '7': row3[0], '8': row3[1], '9': row3[2], '*': row3[3],
+    'C': row4[0], '0': row4[1], '=': row4[2], '/': row4[3],
+  }
+}
+
+function pushButtons(app: ReactTestInstance, button: Button[]): ReactTestInstance {
+  const mapping = makePads(app)
+  button.map((label) => mapping[label].props.onPress())
+  return app
+}
+
+export function pushed(button: Button[]): ReactTestInstance {
+  return pushButtons(
+    Renderer.create(<App />).root,
+    button
+  )
+}
