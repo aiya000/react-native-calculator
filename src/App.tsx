b1ac01bdcf6c84bbbfe0413522ef7490f2aa4fec
aiya000 <aiya000.develop@gmail.com>
2019-03-12 18:30:41 +0900

    Today's WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,76 @@
 import * as React from 'react'
+import * as data from './data'
+import * as nullable from './data/null'
 import { KeyPad } from './KeyPad'
-import { Operation, Digit, Input } from './data'
+import { Operation, NumericOperation, Digit, Input } from './data'
 import { StyleSheet, Text, View } from 'react-native'
 
-interface State {
+/**
+ * Represents calculator's the one of expression.
+ *
+ * ```typescript
+ * // 1 / 3
+ * {applyee: 1, apply: Operation.Div, applyer: 3}
+ * ```
+ */
+export interface State {
   /**
    * A displayed calculated result.
    */
-  result: number
+  applyee: number
 
-  /**
-   * User input state.
-   *
-   * e.g.
-   * When a user inputs 4, 2, and +,
-   * this has `[42, +]`.
-   *
-   * Or when a user inputs 4, 2, +, 10, =,
-   * this has `[null, null]`.
-   */
-  stack: [number | null, Operation | null]
+  applyer: number
+  apply: NumericOperation | null
+}
+
+/**
+ * A shorthand to `State`.
+ */
+function state(
+  applyee: number,
+  applyer: number,
+  apply: NumericOperation | null,
+) {
+  return {applyee, applyer, apply}
 }
 
 const initialState: State = {
-  result: 0,
-  stack: [null, null],
+  applyee: 0,
+  applyer: 0,
+  apply: null,
+}
+
+function resolveInput(self: State, x: Input): State {
+  if (data.isOperation(x)) {
+    return resolveOperation(self, x)
+  } else if (data.isDigit(x)) {
+    return resolveDigit(self, x)
+  } else {
+    throw Error(`Unknown input: ${x}`)
+  }
+}
+
+function resolveOperation(self: State, x: Operation): State {
+  switch (x) {
+    case Operation.Clear:
+      return initialState
+    case Operation.Resolve:
+      return nullable.fold(self.apply,
+        (apply) => {
+          const result = data.toFunction(apply)(self.applyee, self.applyer)
+          return state(result, 0, null)
+        },
+        () => state(self.applyer, 0, null),
+      )
+  }
+  if (self.apply !== null) {
+    return {
+
+    }
+  }
+}
+
+function resolveDigit(self: State, x: Digit): State {
 }
 
 const styles: any = StyleSheet.create({
@@ -34,7 +80,7 @@ const styles: any = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  resultText: {
+  applyeeText: {
     fontSize: 40,
     backgroundColor: 'silver',
     borderColor: 'black',
@@ -50,13 +96,21 @@ export default class App extends React.Component<{}, State> {
   public render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.resultText}>{this.state.result}</Text>
+        <View style={styles.text}>
+          <Text style={styles.applyeeText}>{this.state.applyee}</Text>
+          <Text style={styles.applyText}>
+            { nullable.map(this.state.apply, data.show) }
+          </Text>
+          <Text style={styles.applyerText}>{this.state.applyer}</Text>
+        </View>
         <KeyPad onPressKey={this.resolve}/>
       </View>
     )
   }
 
   private resolve(x: Input) {
-    this.state.stack.push(x)
+    this.setState(
+      resolveInput(this.state, x)
+    )
   }
 }
